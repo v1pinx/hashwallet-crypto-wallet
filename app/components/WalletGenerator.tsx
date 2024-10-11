123db187a51b4ba19df6f22f7b79cd12cc2e7854
Vipin Kumar <vkprajapati665@gmail.com>
2024-10-11 16:39:04 +0530

    update

--- app/components/WalletGenerator.tsx
+++ app/components/WalletGenerator.tsx
@@ -8,9 +8,9 @@ import { ethers } from 'ethers';
 import bs58 from "bs58";
 import toast, { Toaster } from "react-hot-toast";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClipboard, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
-import { tr } from 'framer-motion/client';
+import 'remixicon/fonts/remixicon.css';
 
 interface Wallet {
     publicKey: string;
@@ -26,8 +26,6 @@ const WalletGenerator = () => {
     const [accountIndex, setAccountIndex] = useState(0);
     const [inputMnemonic, setInputMnemonic] = useState<string>("");
 
-    // Generate Mnemonic once and store it
-    
     const handleBlockchainSelect = (blockchain: string) => {
         console.log(blockchain);
         setBlockchainType(blockchain);
@@ -37,16 +35,15 @@ const WalletGenerator = () => {
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputMnemonic(event.target.value);
     }
-    
+
     const generateBtnClicked = () => {
-        if(inputMnemonic.trim() === ""){
+        if (inputMnemonic.trim() === "") {
             const newMnemonic = bip39.generateMnemonic();
             setMnemonicWords(newMnemonic.split(" "));
             console.log(newMnemonic);
-        }
-        else{
+        } else {
             const words = inputMnemonic.split(" ");
-            if(words.length !== 12){
+            if (words.length !== 12) {
                 toast.error("Please enter 12 words separated by space.");
                 return;
             }
@@ -61,16 +58,12 @@ const WalletGenerator = () => {
             return;
         }
         const mnemonicString = mnemonic.join(' ');
-        
-        generateWalletFromMnemonic(mnemonicString, blockchainType, accountIndex);
 
+        generateWalletFromMnemonic(mnemonicString, blockchainType, accountIndex);
         setAccountIndex(accountIndex + 1);
-        setIsGenerated(true);
     }
 
-    // Copy Mnemonic to Clipboard
     const handleCopyToClipboard = () => {
-
         const mnemonicString = mnemonic.join(' ');
         navigator.clipboard.writeText(mnemonicString)
             .then(() => {
@@ -79,15 +72,10 @@ const WalletGenerator = () => {
             .catch((error) => {
                 console.error("Failed to copy mnemonic to clipboard: ", error);
                 toast.error("Failed to copy mnemonic to clipboard.");
-            })
+            });
     }
 
-    // Generate Wallet from Mnemonic
-    const generateWalletFromMnemonic = (
-        mnemonic: string,
-        pathType: string,
-        accountIndex: number
-    ) => {
+    const generateWalletFromMnemonic = (mnemonic: string, pathType: string, accountIndex: number) => {
         const seedBuffer = bip39.mnemonicToSeedSync(mnemonic);
         const path = `m/44'/${pathType}'/0'/${accountIndex}'`;
         const { key: derivedSeed } = derivePath(path, seedBuffer.toString('hex'));
@@ -95,15 +83,12 @@ const WalletGenerator = () => {
         let publicKeyEncoded: string;
         let privateKeyEncoded: string;
 
-        if (pathType == '501') {
-            // Solana Seed
+        if (pathType === '501') {
             const { secretKey } = nacl.sign.keyPair.fromSeed(derivedSeed);
             const keyPair = Keypair.fromSecretKey(secretKey);
-
             privateKeyEncoded = bs58.encode(secretKey);
             publicKeyEncoded = keyPair.publicKey.toBase58();
-        } else if (pathType == '60') {
-            // Ethereum Seed
+        } else if (pathType === '60') {
             const privateKey = Buffer.from(derivedSeed).toString('hex');
             privateKeyEncoded = privateKey;
 
@@ -123,23 +108,18 @@ const WalletGenerator = () => {
         setWallets((preWallets) => [...preWallets, wallet]);
     }
 
-    const PrivateKeyDisplay = ({ privateKey }:  { privateKey: string }) => {
+    const PrivateKeyDisplay = ({ privateKey }: { privateKey: string }) => {
         const [isVisible, setIsVisible] = useState(false);
-    
+
         return (
             <div>
-                <p className='cursor-pointer' onClick={() => {
-                        setIsVisible(!isVisible);
-                    }}>
+                <p className='cursor-pointer' onClick={() => setIsVisible(!isVisible)}>
                     <strong>Private Key:</strong> {isVisible ? privateKey : '•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••'}
                 </p>
             </div>
         );
     };
-    
 
-    
-    
     const clearWallets = () => {
         setWallets([]);
         setMnemonicWords([]);
@@ -149,102 +129,86 @@ const WalletGenerator = () => {
     }
 
     return (
-        <div className="p-4">
+        <div className="bg-zinc-900 min-h-screen flex flex-col justify-center items-center">
             <Toaster />
             {!blockchainType ? (
-
-                <div>
-                    <div className="text-5xl font-bold">
-                        Hash Wallet Mnemonic Generator
-                    </div>
-                    <div className="text-2xl font-medium mt-2">
-                        Choose a blockchain to get started.
-                    </div>
-                    <div>
+                <div className='flex flex-col justify-center items-center h-screen'>
+                    <i className="ri-wallet-fill text-5xl text-violet-800"></i>
+                    <div className="text-white text-4xl font-bold flex items-center mt-4">Welcome to Hash Wallet</div>
+                    <div className="text-white text-center font-medium text-2xl mt-2">Choose a blockchain to get started.</div>
+                    <div className='mt-24 text-white flex flex-col gap-4'>
                         <button
                             onClick={() => handleBlockchainSelect('501')} // Solana
                             type="button"
-                            className="border px-4 py-2 bg-black text-white rounded mx-2"
+                            className="bg-white text-black px-16 py-3 rounded-xl font-semibold text-sm hover:bg-gray-400 transition"
                         >
                             Solana
                         </button>
                         <button
                             onClick={() => handleBlockchainSelect('60')} // Ethereum
                             type="button"
-                            className="border px-4 py-2 bg-white text-black rounded mx-2"
+                            className="bg-zinc-700 px-16 py-3 rounded-xl font-semibold text-sm hover:bg-zinc-800 transition"
                         >
                             Ethereum
                         </button>
                     </div>
                 </div>
-            ) : (
-                <div>
-                    Select a blockchain: {blockchainType === '501' ? 'Solana' : 'Ethereum'}
+            ) : null}
+
+            {!mnemonic.length && blockchainType !== null && (
+                <div className='h-screen flex flex-col justify-center items-center'>
+                    <div className='text-white font-semibold'>
+                        Selected blockchain: {blockchainType === '501' ? 'Solana' : 'Ethereum'}
+                    </div>
+                    <div className="text-white text-4xl font-bold flex items-center mt-2">Secret Recovery Phase</div>
+                    <div className="text-white text-center font-medium text-2xl mt-2">Save these words in a safe place.</div>
+                    <div className='mt-4 flex gap-2'>
+                        <input
+                            className='border border-gray-400 px-4 py-2 rounded w-76 sm:w-96'
+                            type="text"
+                            onChange={handleInputChange}
+                            placeholder="Enter your secret phrase ( or leave blank to generate )"
+                        />
+                        <button
+                            type='button'
+                            className='px-4 py-2 bg-violet-800 text-white rounded font-semibold hover:bg-violet-700 transition'
+                            onClick={generateBtnClicked}
+                        >
+                            Generate
+                        </button>
+                    </div>
                 </div>
             )}
 
-            {!mnemonic.length && blockchainType != null && (
-                <div>
-                <div>
-                    <div className="text-5xl font-bold">
-                        Secret Recovery Phase
-                    </div>
-                    <div className="text-2xl font-medium mt-2">
-                        Save these words in safe place.
-                    </div>
-                </div>
-                <h3 className="text-md font-semibold">
-                    Selected Blockchain: {blockchainType === '501' ? 'Solana' : 'Ethereum'}
-                </h3>
-                <div className='mt-4 flex gap-2'>
-                    <input
-                        className='border px-4 py-2 rounded w-full'
-                        type="password"
-                        onChange={() => handleInputChange}
-                        placeholder="Enter your secret phrase ( or leave blank to generate )"
-                    />
-                    <button
-                        type='button'
-                        className='border px-4 py-2 bg-green-500 text-white rounded'
-                        onClick={generateBtnClicked}
+            {isGenerated && (
+                <>
+                    <h3 className="text-lg font-bold text-white">Your Mnemonic:</h3>
+                    <motion.div
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5 }}
+                        className="mt-4 p-4 bg-zinc-700 rounded-lg w-[32rem]"
                     >
-                        Generate
-                    </button>
-                </div>
-            </div>
-            ) }
-
-            
-
-
-
-            {mnemonic.length > 0 && mnemonic[0].trim() && isGenerated && (
-                <motion.div
-                    initial={{ opacity: 0, y: -20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
-                    className="mt-4"
-                >
-                    <div className="mt-4">
-                        <h3 className="text-lg font-bold">Your Mnemonic:</h3>
-                        <div className="flex flex-wrap gap-2 mt-2">
+                        <div className="grid grid-cols-4 gap-2 mt-2">
                             {mnemonic.map((word, index) => (
-                                <span key={index} className="bg-gray-200 px-2 py-1 rounded text-black">
+                                <span key={index} className="bg-gray-200 px-2 py-1 rounded text-black text-center">
                                     {word}
                                 </span>
                             ))}
                         </div>
+                        <div className='flex justify-between mt-2'>
+
                         <button
                             onClick={handleCopyToClipboard}
                             type="button"
-                            className="flex items-center bg-gray-500 text-white p-2 mt-2 rounded hover:bg-gray-600"
+                            className="flex items-center bg-gray-500 text-white p-2 rounded hover:bg-gray-600 transition"
                         >
                             <FontAwesomeIcon icon={faClipboard} className="h-5 w-5" />
                         </button>
-
-                        <button className='py-2 px-4 bg-green-500 border text-white mt-2' type='button' onClick={handleWalletGenerate}>Add Wallet</button>
-                    </div>
-                </motion.div>
+                        <button className='px-4 py-2 bg-violet-800 text-white rounded font-semibold hover:bg-violet-700 transition' type='button' onClick={handleWalletGenerate}>Add Wallet</button>
+                        </div>
+                    </motion.div>
+                </>
             )}
 
             {wallets.length > 0 && (
@@ -254,7 +218,7 @@ const WalletGenerator = () => {
                     transition={{ duration: 0.5 }}
                     className="mt-6"
                 >
-                    <h3 className="text-lg font-bold">Generated Wallets:</h3>
+                    <h3 className="text-lg font-bold text-white">Generated Wallets:</h3>
                     {wallets.map((wallet, index) => (
                         <div key={index} className="p-4 bg-gray-100 rounded mt-4">
                             <h4 className="text-md font-semibold">Wallet {index + 1}:</h4>
@@ -268,6 +232,5 @@ const WalletGenerator = () => {
         </div>
     );
 }
-
 
 export default WalletGenerator;
